6671c448621f36eab2c895e8344e5db0394a1352
Lindsey Armstrong <linmicarm@gmail.com>
2026-06-05 14:26:18 -0400

    added more js functionality

--- etchASketch/script.js
+++ etchASketch/script.js
@@ -4,8 +4,23 @@ function createGrid(size) {
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.style.width = (960 / size) + "px";
+    square.style.height = (960 / size) + "px";
+
+    square.addEventListener("mouseover", () => {
+      square.classList.add("hovered");
+    });
+
     container.appendChild(square);
   }
 }
 
 createGrid(16);
+
+const resize = document.querySelector("#resize");
+
+resize.addEventListener("click", () => {
+  const newSize = parseInt(prompt("How many squares per side?"));
+  container.innerHTML = "";
+  createGrid(newSize);
+});
