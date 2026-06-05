625ff08959109343cad1f6daba655ca14c547f7a
Lindsey Armstrong <linmicarm@gmail.com>
2026-06-05 14:41:34 -0400

    Add etch-a-sketch extra credit: random colors and progressive darkening

--- etchASketch/script.js
+++ etchASketch/script.js
@@ -1,14 +1,26 @@
 const container = document.querySelector("#container");
 
+function randomColor() {
+  return Math.floor(Math.random() * 256);
+}
+
 function createGrid(size) {
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
-    square.style.width = (960 / size) + "px";
-    square.style.height = (960 / size) + "px";
+    square.style.width = 960 / size + "px";
+    square.style.height = 960 / size + "px";
+    square.clicks = 0;
 
     square.addEventListener("mouseover", () => {
-      square.classList.add("hovered");
+      square.clicks += 1;
+
+      const r = randomColor();
+      const g = randomColor();
+      const b = randomColor();
+      square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+
+      square.style.opacity = square.clicks / 10;
     });
 
     container.appendChild(square);
@@ -21,6 +33,10 @@ const resize = document.querySelector("#resize");
 
 resize.addEventListener("click", () => {
   const newSize = parseInt(prompt("How many squares per side?"));
-  container.innerHTML = "";
-  createGrid(newSize);
+  if (newSize >= 1 && newSize <= 100) {
+    container.innerHTML = "";
+    createGrid(newSize);
+  } else {
+    alert("Please enter a number between 1 and 100.");
+  }
 });
